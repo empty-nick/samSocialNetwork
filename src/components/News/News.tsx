707dab8258521fa821ee700dd617a0fefd27b3f7
empty-nick <leard246@gmail.com>
2023-02-02 02:31:06 +0300

    L19 - Routing + new components (News, Music, Settings)

---
+++ src/components/News/News.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import css from "./News.module.css"
+
+const News = () => {
+  return (
+      <div className={css.content}>
+        News
+      </div>
+  )
+}
+
+export default News
---
+++ src/components/Settings/Settings.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import css from "./Settings.module.css"
+
+const Settings = () => {
+  return (
+      <div className={css.content}>
+        Settings
+      </div>
+  )
+}
+
+export default Settings
