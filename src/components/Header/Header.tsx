4c4fb66c191ebcee2b11ce9c89e26fd8dc21123b
empty-nick <leard246@gmail.com>
2023-02-02 03:40:46 +0300

    L20 - NavLink + active className

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import css from "./Header.module.css"
 function Header(){
   return (
       <header className={css.header}>
-        <img src="https://1000logos.net/wp-content/uploads/2016/10/Apple-Logo.png" alt="Apple"/>
+        <img src="https://i.yapx.ru/VcLVV.png" alt="Apple"/>
       </header>
   )
 }
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,15 +1,40 @@
 import React from "react"
 import css from "./Sidebar.module.css"
+import {NavLink} from "react-router-dom";
 
 function Sidebar() {
   return (
       <nav className={css.sidebar}>
-        <div className={css.item}><a href="/profile">Profile</a></div>
-        <div className={`${css.item} ${css.active}`}><a href="/messages">Messages</a></div>
-        <div className={css.item}><a href="/news">News</a></div>
-        <div className={css.item}><a href="/music">Music</a></div>
+        <div className={css.item}>
+          <NavLink to="/profile" className={state => state.isActive ? css.active : css.notActive} >
+            Profile
+          </NavLink>
+        </div>
 
-        <div className={`${css.item} ${css.settings}`}><a href="/settings">Settings</a></div>
+        <div className={`${css.item} ${css.active}`}>
+          <NavLink to="/messages" className={state => state.isActive ? css.active : css.notActive} >
+            Messages
+          </NavLink>
+        </div>
+
+        <div className={css.item}>
+          <NavLink to="/news" className={state => state.isActive ? css.active : css.notActive} >
+            News
+          </NavLink>
+        </div>
+
+        <div className={css.item}>
+          <NavLink to="/music" className={state => state.isActive ? css.active : css.notActive} >
+            Music
+          </NavLink>
+        </div>
+
+        <div className={`${css.item} ${css.settings}`}>
+          <NavLink to="/settings" className={state => state.isActive ? css.active : css.notActive} >
+            Settings
+          </NavLink>
+        </div>
+
       </nav>
   )
 }
